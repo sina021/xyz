4e98362b1e297d40ee075e7c5bffb4ce6f513168
sina021 <sina021370@gmail.com>
2016-05-15 10:50:31 +0430

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,1 +1,33 @@
 
+do
+local function run(msg, matches)
+local bot_id = 180836680
+local x = 198434641
+local y = 205692043
+local z = 201318555
+local w = 198964014
+
+    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(w) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..w, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^[!/](bye)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
