49f055c87f63c012034a43db51f94679f6aee838
sina021 <sina021370@gmail.com>
2016-05-15 11:39:12 +0430

    Add files via upload

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,25 +1,16 @@
-
 do
 local function run(msg, matches)
-local bot_id = 180836680
-local x = 198434641
-local y = 205692043
-local z = 201318555
-local w = 198964014
+local bot_id = 144629652
+local y = 206003412
+local z = 219602465
 
     if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
-    end
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false
      elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
     end
      elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
-    end
-     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(w) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..w, ok_cb, false)
     end
 end
  
